653ec8e208ccd7374117f0517ea5a213089f70c7
박은정 <beanlove97@gmail.com>
2021-12-07 21:30:19 +0900

    Add: React Part1 02, 03, 04 수강완료

--- React/react_blog/src/App.js
+++ React/react_blog/src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const posts = '강남고기맛집';
+
+  let [post, setPost] = useState('남자 코트 추천');
+  let [글제목, 글제목변경] = useState(['남자 코드 추천', '강남 우동 맛집', '여자 코드 추천']);
+  let [따봉수, 따봉수변경] = useState(0)
+
+  // const post = '강남고기맛집';
   return (
     <div className="App">
-      <div className='black-nav'>
+      <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <h4>{posts}</h4>
+      <div className="list">
+        <h3>
+          {글제목[0]} <span>👍🏻</span> {따봉수}
+        </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>3월 18일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>4월 28일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
